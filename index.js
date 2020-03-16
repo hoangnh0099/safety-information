683cecb619ee5aa3b94c36885dcb84c1161027ac
Nguyen Huy Hoang <hoangnh0099@gmail.com>
2020-03-16 14:01:14 +0700

    Update

---
+++ index.js
@@ -0,0 +1,31 @@
+/**
+ *
+ * @flow
+ */
+
+/**
+ * Nghiêm Đình Trường
+ * Bài 1: Hệ mã dịch vòng (Shift Cipher)
+ */
+
+const shiftCipherEncode = (text: string): string => {
+  return '';
+};
+
+/**
+ * Nguyễn Quang Quý
+ * Bài 2: Hệ mã hóa thay thế (Substitution Cipher)
+ */
+
+const substitutionCipher = (text: string): string => {
+  return '';
+};
+
+/**
+ * Nguyễn Huy Hoàng
+ * Bài 3: Hệ mã Affine
+ */
+
+const affineEncrypt = (text: string): string => {
+  return '';
+};
